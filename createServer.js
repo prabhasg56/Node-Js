ffb7f90fe3a7d9437cf13793f5017155d7262c85
PRABHAKAR MANDAL <86845434+prabhasg56@users.noreply.github.com>
2023-11-21 12:59:58 +0530

    res, re

--- createServer.js
+++ createServer.js
@@ -1,7 +1,42 @@
 import http from 'http';
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    // console.log(req.headers, req.url, req.method);
+
+    let url = req.url;
+    if(url==="/message"){
+        res.write('<html>')
+        res.write('<head><title>Enter message </title> </head>')
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message" ></input>  <button type="submit"> submit </button></form> </body>')
+        res.write('</html>')
+       return  res.end()
+    }else if(url==="/home"){
+        res.write('<html>')
+        res.write('<head><title>Enter message </title> </head>')
+        res.write('<body><h3> welcome to home </h3> </body>')
+        res.write('</html>')
+       return  res.end()
+    }else if(url==="/about"){
+        res.write('<html>')
+        res.write('<head><title>Enter message </title> </head>')
+        res.write('<body><h3> welcome to about </h3> </body>')
+        res.write('</html>')
+       return  res.end()
+    }else if(url==="/node"){
+        res.write('<html>')
+        res.write('<head><title>Enter message </title> </head>')
+        res.write('<body><h3> welcome to node </h3> </body>')
+        res.write('</html>')
+       return  res.end()
+    }
+
+
+    res.setHeader('Content-type', 'text/html');
+    res.write('<html>')
+    res.write('<head><title>First node page </title> </head>')
+    res.write('<body><h2> Hello gd morning </h2> </body>')
+    res.write('</html>')
+    res.end()
 });
 
 server.listen(3000, ()=>{
